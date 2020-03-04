aae45e76db381754dbf8fcb15274e7f4046b5957
Ales Kavcic <ales.kavcic@comland.si>
2020-03-04 10:05:33 +0100

    Base url set to: https://www.mesimedical.com/

--- app/src/main/java/com/example/mesimedical/common/Constants.kt
+++ app/src/main/java/com/example/mesimedical/common/Constants.kt
@@ -2,9 +2,8 @@ package com.example.album.common
 
 class Constants {
     companion object {
-        //const val BASE_URL = "https://www.mesimedical.com/"
-        const val BASE_URL ="https://www.mesimedical.com/peripheral-arterial-disease-and-diabetes/"
+        const val BASE_URL = "https://www.mesimedical.com/"
+        //const val BASE_URL ="https://www.mesimedical.com/peripheral-arterial-disease-and-diabetes/"
         //const val BASE_URL ="https://www.mesimedical.com/support/mesi-mtablet/"
-        const val HOST_URL = "www.mesimedical.com"
     }
 }
